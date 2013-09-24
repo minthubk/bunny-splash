cf21c69bc3fe7c77be44aab4e569a5e8cff12651
Damien Mayance <dmayance@gmail.com>
2013-09-24 16:42:13 +0200

    Not working well flying platforms

--- Assets/Scripts/Props/FlyingPlatformScript.cs
+++ Assets/Scripts/Props/FlyingPlatformScript.cs
@@ -1,15 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FlyingPlatformScript : MonoBehaviour {
+public class FlyingPlatformScript : MonoBehaviour
+{
+    public Vector3 Direction;
+    public float Amplitude = 1;
+    public float Speed = 2f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Vector3 mStartPosition;
+    private Vector3 mTarget;
+    private Vector3 mDistanceBetweenCenterAndCollider;
+
+    void Awake()
+    {
+        acquireTarget();
+
+        mDistanceBetweenCenterAndCollider = collider.bounds.center - transform.position;
+    }
+
+    private void acquireTarget()
+    {
+        mStartPosition = transform.position;
+        mTarget = mStartPosition + (Direction * Amplitude);
+    }
+
+    void Update()
+    {
+        // Check if collider reached the target
+        float distance = Vector3.Distance(collider.bounds.center, mTarget + mDistanceBetweenCenterAndCollider);
+
+        if (distance > 0.15f)
+        {
+            // Move
+            Vector3 movement = Direction * Speed * Time.deltaTime;
+            transform.Translate(movement);
+        }
+        else
+        {
+            // Swap target
+            Direction.x = -Direction.x;
+            Direction.y = -Direction.y;
+
+            acquireTarget();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(mStartPosition + mDistanceBetweenCenterAndCollider, mTarget + mDistanceBetweenCenterAndCollider);
+    }
 }
