062f4765b190bf2b2cc5c072c8669fa89dc4b903
Damien Mayance <dmayance@gmail.com>
2013-09-30 11:26:08 +0200

    Putting platforms stuff in folders and renaming

---
+++ Assets/Scripts/Tools/GravityController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Set the gravity on Awake for the whole scene
+/// </summary>
+public class GravityController : MonoBehaviour
+{
+    public Vector3 Gravity = new Vector3(0, -9.81f, 0);
+
+    void Awake()
+    {
+        Physics.gravity = Gravity;
+    }
+
+}
